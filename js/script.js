e50eed8d74b708cf68f79b73631bc2e0a913a257
ingenioust91 <ingecendrawatisalim@gmail.com>
2025-08-01 14:48:24 +0700

    add transition on hamburger menu

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function hamburgeropen() {
+    var burger = document.getElementById("menubar");
+    
+    if (window.innerWidth >= 1024){
+        if (burger.style.transform === "translateY(-100%)")
+            {document.getElementById("menubar").style.transform = "translateY(0)";}
+        else 
+            {document.getElementById("menubar").style.transform = "translateY(-100%)"}
+    }
+    else {
+        if (burger.style.transform === "translateX(-100%)")
+            {document.getElementById("menubar").style.transform = "translateX(0)";}
+        else
+            {document.getElementById("menubar").style.transform = "translateX(-100%)";}
+    
+    }
+
+
+}
+
+
+
+function hamburgerclose() {
+    if (window.innerWidth >= 1024)
+    {document.getElementById("menubar").style.transform = "translateY(-100%)"}
+    else {document.getElementById("menubar").style.transform = "translateX(-100%)";}
+}
